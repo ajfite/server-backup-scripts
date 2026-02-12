e296e76b9fbf45defb6dc8a1525f697401de3691
AJ Fite <me@ajfite.com>
2026-02-11 19:49:20 -0800

    fiddling around

--- server_backup_scripts/backup_server.py
+++ server_backup_scripts/backup_server.py
@@ -1,9 +1,52 @@
 #!/usr/bin/env python3
+import click
+import os
+
+DEFAULT_BACKUP_DIR: str = "/opt/backup-management"
 
 
-def main():
-    print("Hello World")
+@click.command()
+@click.option(
+    "-d",
+    "--backup-dir",
+    type=click.Path(),
+    default=DEFAULT_BACKUP_DIR,
+    help="backup directory that should be managed",
+)
+def main(backup_dir: str):
+    print(f"Hello World {backup_dir}")
+
+
+class Collect:
+    def __init__(self):
+        self.dir_manager = DirManager()
+
+    def gitlab(self):
+        pass
+
+    def postgres(self):
+        pass
+
+    def vaultwarden(self):
+        pass
+
+    def etc(self):
+        pass
+
+
+class DirManager:
+    backups: list[str] = list()
+
+    def __init__(self, backup_dir: str = DEFAULT_BACKUP_DIR):
+        self.backup_dir = backup_dir
+
+    def get_backup_dirs(self):
+        for f in os.listdir(self.backup_dir):
+            full_path = os.path.join(self.backup_dir, f)
+
+            if os.path.isdir(full_path):
+                self.backups.append(full_path)
 
 
 if __name__ == "__main__":
-    main()
+    main()  # noqa: E1120 Click will handle this for us
---
+++ server_backup_scripts/update_server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def main():
+    print("Hello World")
+
+
+class update:
+    def vaultwarden(self):
+        pass
+
+    def apt(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
