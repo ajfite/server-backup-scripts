4cd98771b00394cf667a1113bfaf2604d92e7b24
AJ Fite <me@ajfite.com>
2026-02-21 00:18:56 -0800

    slight rework to process running to prevent null returncodes

--- server_backup_scripts/process_utils.py
+++ server_backup_scripts/process_utils.py
@@ -7,11 +7,13 @@ def run_process_with_stdout(cmd: list[str]) -> tuple[str, int]:
     process = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
+    out, error = process.communicate()
+    process.wait()
+
+    logger.error(error)
     logger.info(f"Completed {cmd} with status {process.returncode}")
 
     if process.returncode != 0:
         logger.critical(f"Command {cmd} did not run successfully :(")
 
-    out, error = process.communicate()
-    logger.error(error)
     return out, process.returncode
